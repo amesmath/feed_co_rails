a09ec6b19bc8a6cc16520027f6728aaaf0db4d81
David Ames <dames@acivilate.com>
2024-07-27 22:53:56 -0700

    remove Category model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,4 @@
 class Product < ApplicationRecord
-  belongs_to :category
   belongs_to :company
   validates :name, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0 }, allow_nil: true
