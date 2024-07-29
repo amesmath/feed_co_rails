8486c94618273a87372ccb9926fdeaa346fb4ae0
David Ames <dames@acivilate.com>
2024-07-29 01:17:30 -0700

    add line chart

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -46,30 +46,14 @@ class DashboardController < ApplicationController
   end
 
   def purchase_order_data_for_chart
-    # Get all PurchaseOrders where company_id is one of the internal_company_ids
-    purchase_orders = PurchaseOrder.where(company_id: internal_company_ids).limit(30)
+    @purchase_orders = PurchaseOrder.where(company_id: internal_company_ids).limit(120)
 
-    # Initialize a hash to store the purchase order count by product and month
-    purchase_order_data = Hash.new { |hash, key| hash[key] = Array.new(6, 0) }
-
-    # Define the last six months
-    last_six_months = (0..5).map { |i| (Date.today - i.months).strftime('%B') }.reverse
-
-    purchase_orders.each do |po|
-      month = po.order_date.strftime('%B')
-      index = last_six_months.index(month)
-      next if index.nil? # Skip if the month is not in the last six months
-
-      po.products.each do |product|
-        product_name = product.name
-        purchase_order_data[product_name][index] += 1
-      end
+    # Format data for Highcharts
+    @chart_data = @purchase_orders.map do |po|
+      {
+        name: po.created_at.strftime('%Y-%m-%d'), # Assuming purchase order has a created_at attribute
+        data: po.total_amount # Assuming purchase order has an amount attribute
+      }
     end
-
-    # Return data formatted for HighCharts
-    {
-      categories: last_six_months,
-      series: purchase_order_data.map { |product_name, data| { name: product_name, data: data } }
-    }
   end
 end
